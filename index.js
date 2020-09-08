db1ffbdf05f61556af4b389407b95f59def5d120
carefree-request-1153 <sgold5757@gmail.com>
2020-09-08 23:52:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ function removeFromCart(item) {
       delete cart[i]
       return cart
     }
-    return `That item is not in your cart`
   }
+  return `That item is not in your cart`
 }
 
 function placeOrder(cardNumber) {
