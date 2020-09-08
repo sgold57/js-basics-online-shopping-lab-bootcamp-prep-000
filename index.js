118fe9212f80ed9d8578b8aa3914fac0d87e7326
carefree-request-1153 <sgold5757@gmail.com>
2020-09-08 23:55:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      delete cart[i]
+      delete cart.item
       return cart
     }
   }
