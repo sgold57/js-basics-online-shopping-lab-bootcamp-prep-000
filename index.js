11fe2f824cd715242fb4904dc9d98820419f1fda
carefree-request-1153 <sgold5757@gmail.com>
2020-09-08 23:52:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
       return cart
     }
   }
-  return `That item is not in your cart`
+  return `That item is not in your cart.`
 }
 
 function placeOrder(cardNumber) {
