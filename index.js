bd5ea4531d7ccf186987e596b257b909fc5a123d
carefree-request-1153 <sgold5757@gmail.com>
2020-09-08 23:36:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,35 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemPrice = Math.floor((Math.random() * 100) + 1)
+ var newItem = {"itemName": item, "itemPrice": itemPrice}
+ cart.unshift(newItem)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var cartString = "In your cart, you have "
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+  if (cart.length === 1) {
+    cartString = cartString + `${cart[0].itemName} at $${cart[0].itemPrice}.`
+    return cartString
+  }
+  else {
+    for (let i = 0; i < cart.length; i++) {
+      if (i === 0) {
+        cartString = cartString + `${cart[i].itemName} at $${cart[i].itemPrice}`
+      }
+      else if (i === (cart.length - 1)) {
+        cartString = cartString + `, and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+      }
+      else {
+        cartString = cartString + `, ${cart[i].itemName} at $${cart[i].itemPrice}`
+      }
+  }
+  return cartString
+  }
 }
 
 function total() {
