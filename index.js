0857f27280e8cc0af36701327fa040537acba444
carefree-request-1153 <sgold5757@gmail.com>
2020-09-09 00:13:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   else {
-    finalTotal = total()
+    var finalTotal = total()
     cart = []
     return `Your total cost is $${finalTotal}, which will be charged to the card ${cardNumber}.`
   }
